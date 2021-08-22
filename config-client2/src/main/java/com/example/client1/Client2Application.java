72396376013871f3724d35517021b5be09f34b0b
Thomas Andre <thomasandre84@gmail.com>
2021-08-22 19:08:13 +0200

    add Clients

---
+++ config-client2/src/main/java/com/example/client1/Client2Application.java
@@ -0,0 +1,24 @@
+package com.example.client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class Client2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Client2Application.class, args);
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<Mono<String>> hello() {
+		return ResponseEntity.ok(Mono.just("Hello Client2"));
+	}
+
+}
